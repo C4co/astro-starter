59b86d4d6643d3154f185a6d52316556168b06e5
Carlos Costa <carllos.nc@gmail.com>
2022-10-15 14:40:46 -0300

    init

---
+++ astro.config.mjs
@@ -0,0 +1,20 @@
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
+
+// https://astro.build/config
+export default defineConfig({
+  markdown: {
+    shikiConfig: {
+      theme: "github-light",
+    },
+  },
+
+  integrations: [tailwind(), mdx()],
+});
