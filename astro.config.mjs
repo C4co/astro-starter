068c11418a4eda1c07b4bfe575cb4744b7b42834
Carlos Costa <carllos.nc@gmail.com>
2022-10-15 22:01:53 -0300

    config: add svelte and set shiki theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,14 @@
 import { defineConfig } from 'astro/config'
-
-// https://astro.build/config
 import tailwind from '@astrojs/tailwind'
-
-// https://astro.build/config
 import mdx from '@astrojs/mdx'
-
-// https://astro.build/config
+import svelte from '@astrojs/svelte'
 
 // https://astro.build/config
 export default defineConfig({
   markdown: {
     shikiConfig: {
-      theme: 'monokai',
+      theme: 'material-palenight',
     },
   },
-
-  integrations: [tailwind(), mdx()],
+  integrations: [tailwind(), mdx(), svelte()],
 })
