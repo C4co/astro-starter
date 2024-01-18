b231f8bc1105a8a9a4fcb46ff8c92b3c482ab385
Carlos Costa <carllos.nc@gmail.com>
2024-01-17 23:31:55 -0300

    Add new components to index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,4 @@
-export { default as Menu } from './Menu.astro'
+export { default as Navbar } from './Navbar.astro'
 export { default as Footer } from './Footer.astro'
+export { default as Header } from './Header.astro'
+export { default as Logo } from './Logo.astro'
